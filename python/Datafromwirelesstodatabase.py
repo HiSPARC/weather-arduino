0e0576d75246237ee15fa109a73f5fef8cc9e719
Arne de Laat <adelaat@nikhef.nl>
2015-06-01 20:41:48 +0200

    Fix flake8 errors

--- python/Datafromwirelesstodatabase.py
+++ python/Datafromwirelesstodatabase.py
@@ -1,18 +1,13 @@
 import serial
 import datetime
 import time
-import collections
 import numpy
-import sys, serial
-import time
-import MySQLdb
 import hashlib
 import requests
 import logging
 
 import cPickle as pickle
 
-from time import sleep
 from requests.exceptions import ConnectionError, Timeout
 
 logger = logging.getLogger(__name__)
@@ -20,26 +15,47 @@ logger = logging.getLogger(__name__)
 DATASTORE_URL = "http://frome.nikhef.nl/hisparc/upload"
 
 
+class StorageError(Exception):
+
+    """Base error class."""
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return "Error storing events (%s)" % self.msg
+
+
+class UploadError(StorageError):
+
+    """Error uploading events."""
+
+    def __str__(self):
+        return "Error uploading events (%s)" % self.msg
+
+
 class Measurement(object):
 
     def __init__(self, output):
 
-        # Datastore assumes date, time, temp_inside (detector), tempOutside, humidityInside ,
-        # humidityOutside, barometer, windDir, windSpeed, solarRad, UV, ET, rainRate
-        # heatIndex, dewPoint, windChill
+        # Datastore assumes date, time, temp_inside (detector), tempOutside,
+        # humidityInside, humidityOutside, barometer, windDir, windSpeed,
+        # solarRad, UV, ET, rainRate, heatIndex, dewPoint, windChill
 
-        self.temp_inside, self.temp_outside, self.humidity_outside, self.barometer = output
+        self.temp_inside, self.temp_outside, self.humidity_outside, \
+            self.barometer = output
 
-        Tdew = self.dampdruk_calc(self.temp_outside, self.humidity_outside, self.barometer)
+        Tdew = self.dampdruk_calc(self.temp_outside, self.humidity_outside,
+                                  self.barometer)
 
         # As we do not measure these weather variables we set them to '-999'
-        # If the weatherstation does measure these variables we can add them to the list
-        # extract them from the Arduino output.
+        # If the weatherstation does measure these variables we can add
+        # them to the list extract them from the Arduino output.
 
         self.wind_dir = '-999'
         self.humidity_inside = '-999'
         self.wind_speed = '-999'
-        self.solar_rad  = '-999'
+        self.solar_rad = '-999'
         self.uv = '-999'
         self.evapotranspiration = '-999'
         self.rain_rate = '-999'
@@ -51,17 +67,17 @@ class Measurement(object):
         self.datetime = datetime.datetime.now()
         self.nanoseconds = 0
 
-
     def dampdruk_calc(self, Tout, Hum_out, baro):
-        RH = Hum_out/100  #calculate relative humidity
+        RH = Hum_out / 100  # calculate relative humidity
 
-        # calculate vaporpressure, Dewpoint: Formula from Vantage Pro Davis instruments
+        # calculate vaporpressure, Dewpoint: Formula from Vantage Pro Davis
+        # instruments
 
-        dampdruk = RH * 6.112 * numpy.exp((17.62 * Tout)/(Tout + 243.12))
-        Numerator = 243.12 * numpy.log(dampdruk)-440.1
+        dampdruk = RH * 6.112 * numpy.exp((17.62 * Tout) / (Tout + 243.12))
+        Numerator = 243.12 * numpy.log(dampdruk) - 440.1
         Denominator = 19.43 - numpy.log(dampdruk)
         Tdew = Numerator / Denominator
-        Tdew = "%4.1f" %Tdew
+        Tdew = "%4.1f" % Tdew
 
         return Tdew
 
@@ -100,20 +116,29 @@ class NikhefDataStore(object):
                   'nanoseconds': event.nanoseconds}
 
         datalist = []
-        self._add_value_to_datalist(datalist, 'WTR_TEMP_INSIDE', event.temp_inside)
-        self._add_value_to_datalist(datalist, 'WTR_TEMP_OUTSIDE', event.temp_outside)
-        self._add_value_to_datalist(datalist, 'WTR_HUMIDITY_INSIDE', event.humidity_inside)
-        self._add_value_to_datalist(datalist, 'WTR_HUMIDITY_OUTSIDE', event.humidity_outside)
-        self._add_value_to_datalist(datalist, 'WTR_BAROMETER', event.barometer)
+        self._add_value_to_datalist(datalist, 'WTR_TEMP_INSIDE',
+                                    event.temp_inside)
+        self._add_value_to_datalist(datalist, 'WTR_TEMP_OUTSIDE',
+                                    event.temp_outside)
+        self._add_value_to_datalist(datalist, 'WTR_HUMIDITY_INSIDE',
+                                    event.humidity_inside)
+        self._add_value_to_datalist(datalist, 'WTR_HUMIDITY_OUTSIDE',
+                                    event.humidity_outside)
+        self._add_value_to_datalist(datalist, 'WTR_BAROMETER',
+                                    event.barometer)
         self._add_value_to_datalist(datalist, 'WTR_WIND_DIR', event.wind_dir)
-        self._add_value_to_datalist(datalist, 'WTR_WIND_SPEED', event.wind_speed)
+        self._add_value_to_datalist(datalist, 'WTR_WIND_SPEED',
+                                    event.wind_speed)
         self._add_value_to_datalist(datalist, 'WTR_SOLAR_RAD', event.solar_rad)
         self._add_value_to_datalist(datalist, 'WTR_UV', event.uv)
-        self._add_value_to_datalist(datalist, 'WTR_ET', event.evapotranspiration)
+        self._add_value_to_datalist(datalist, 'WTR_ET',
+                                    event.evapotranspiration)
         self._add_value_to_datalist(datalist, 'WTR_RAIN_RATE', event.rain_rate)
-        self._add_value_to_datalist(datalist, 'WTR_HEAT_INDEX', event.heat_index)
+        self._add_value_to_datalist(datalist, 'WTR_HEAT_INDEX',
+                                    event.heat_index)
         self._add_value_to_datalist(datalist, 'WTR_DEW_POINT', event.dew_point)
-        self._add_value_to_datalist(datalist, 'WTR_WIND_CHILL', event.wind_chill)
+        self._add_value_to_datalist(datalist, 'WTR_WIND_CHILL',
+                                    event.wind_chill)
 
         event_list = [{'header': header, 'datalist': datalist}]
         return event_list
@@ -157,7 +182,8 @@ class NikhefDataStore(object):
         else:
             logger.debug("Response from server: %s", r.text)
             if r.text != '100':
-                raise UploadError("Server responded with error code %s" % r.text)
+                raise UploadError("Server responded with error code %s" %
+                                  r.text)
 
     def close(self):
         """Close the datastore."""
@@ -166,27 +192,30 @@ class NikhefDataStore(object):
 
 
 if __name__ == '__main__':
-    # Send to NikhefDatastore(Station_id, password) not known please contact info@hisparc.nl
+    # Send to NikhefDatastore(Station_id, password) not known please contact
+    # info@hisparc.nl
     datastore = NikhefDataStore(599, 'pysparc')
 
-    #location of /USB wireless connection for MAC could be tty instead of cu adress
+    # location of USB wireless connection for Mac could be tty instead of
+    # cu adress
     strPort = '/dev/cu.SLAB_USBtoUART'
     ser = serial.Serial(strPort, baudrate=9600, parity=serial.PARITY_NONE,
-            bytesize=serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE, timeout=1.0)
+                        bytesize=serial.EIGHTBITS,
+                        stopbits=serial.STOPBITS_ONE, timeout=1.0)
     prev_data = [2]
     while True:
         try:
-          data = ser.readline()
-          if data == '':  #check if data is not empty
-            time.sleep(2) # change to higher sleep time
-            continue
-          data = [float(val) for val in data.split(',')]
-          if prev_data != data and len(data) <= 26:
-            prev_data = data
-            measurement = Measurement(data)
-            #print measurement.dew_point
-            datastore.store_event(measurement)
-            time.sleep(1)    # can be changed to higher sleep time
+            data = ser.readline()
+            if data == '':  # check if data is not empty
+                time.sleep(2)  # change to higher sleep time
+                continue
+            data = [float(val) for val in data.split(',')]
+            if prev_data != data and len(data) <= 26:
+                prev_data = data
+                measurement = Measurement(data)
+                # print measurement.dew_point
+                datastore.store_event(measurement)
+                time.sleep(1)    # can be changed to higher sleep time
         except KeyboardInterrupt:
             print ' exiting'
             break
